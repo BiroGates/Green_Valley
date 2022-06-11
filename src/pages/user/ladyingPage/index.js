9407a79c50298f261174cd122ac0da0b712d6a73
Gustavo Alves Vieira <www.guga45@hotmail.com>
2022-06-11 12:10:48 -0300

    Arrumando Landying page responsiva

--- src/pages/user/ladyingPage/index.js
+++ src/pages/user/ladyingPage/index.js
@@ -1,4 +1,4 @@
-    import { motion, useViewportScroll, useTransform }from 'framer-motion'
+import { motion, useViewportScroll, useTransform }from 'framer-motion'
 
 import { Link } from 'react-router-dom'
 
