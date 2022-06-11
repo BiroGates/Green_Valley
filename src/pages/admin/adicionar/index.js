c4600a3e53dd2fadfcae0f2d963d1e5f2bde0b00
Gustavo Alves Vieira <www.guga45@hotmail.com>
2022-06-11 17:37:59 -0300

    Arrumando pagina de adicionar

--- src/pages/admin/adicionar/index.js
+++ src/pages/admin/adicionar/index.js
@@ -19,17 +19,18 @@ export default function Adicionar() {
             <LeftBar/>
             <div className='addprodutos'>
                 <div className='add-container'>
-                    <div className='add-img'></div>
-                    <form className='add-upload'> <input type='file' onChange={(e) => console.log(e.target.files[0])} className='btn' />  </form>
+                    
+                    <div className='upload-imagem'>
+                        img
+                    </div>
+
                     <form className='add-form' >
                         <div className='row-1'>
-                            <label> titulo </label>
-                            <input type="text" name='nm_produto' value={formData.nm_produto} onChange={(e) => setFormData({ ...formData, nm_produto: e.target.value })} />
+                            <input placeholder='Ex: Sonho' type="text" name='nm_produto' value={formData.nm_produto} onChange={(e) => setFormData({ ...formData, nm_produto: e.target.value })} />
                         </div>
                         <div className='row-2'>
                             <div className='item-1'>
-                                <label> preco </label>
-                                <input type="number" name='nr_preco' value={formData.nr_preco} onChange={(e) => setFormData({ ...formData, nr_preco: e.target.value })} />
+                                <input placeholder='Ex: 5.50' type="number" name='nr_preco' value={formData.nr_preco} onChange={(e) => setFormData({ ...formData, nr_preco: e.target.value })} />
                             </div>
 
                             <div className='item-2'>
@@ -41,7 +42,7 @@ export default function Adicionar() {
                             </div>
                         </div>
                         <div className='row-3'>
-                            <textarea cols="78" rows="10" value={formData.ds_produto} onChange={(e) => setFormData({ ...formData, ds_produto: e.target.value })} ></textarea>
+                            <textarea placeholder='Ex: Sonho de doce de leite com flocos de coco' value={formData.ds_produto} onChange={(e) => setFormData({ ...formData, ds_produto: e.target.value })} ></textarea>
                         </div>
                         <button className="btn" onSubmit={(e) => e.preventDefault()}> SALVAR </button>
                     </form>
